c685db0b280884ed859567b26256ba0fe1e6e738
Brent Shaffer <betterbrent@google.com>
2018-08-09 23:19:17 -0700

    copies over methods for ExecuteCommandTrait

--- src/TestUtils/ExecuteCommandTrait.php
+++ src/TestUtils/ExecuteCommandTrait.php
@@ -21,6 +21,9 @@ use Symfony\Component\Console\Tester\CommandTester;
 
 trait ExecuteCommandTrait
 {
+    private static $workingDirectory;
+    private static $logger;
+
     use ExponentialBackoffTrait;
 
     private function runCommand($commandName, $args=[])
@@ -48,5 +51,93 @@ trait ExecuteCommandTrait
         }
 
         return $testFunc();
+    }
+
+    public static function setWorkingDirectory($workingDirectory)
+    {
+        self::$workingDirectory = $workingDirectory;
+    }
+
+    /**
+     * run a command
+     *
+     * @param $cmd
+     * @throws \Exception
+     */
+    public static function execute($cmd, $timeout = false)
+    {
+        $process = self::createProcess($cmd, $timeout);
+        self::executeProcess($process);
+
+        return $process->getOutput();
+    }
+
+    /**
+     * Executes a Process and throws an exception
+     *
+     * @param Process $process
+     * @param bool $throwExceptionOnFailure
+     * @throws \Exception
+     */
+    private static function executeProcess(Process $process, $throwExceptionOnFailure = true)
+    {
+        if (self::$logger) {
+            self::$logger->debug(sprintf('Executing: %s', $process->getCommandLine()));
+        }
+
+        $process->run(self::getCallback());
+
+        if (!$process->isSuccessful() && $throwExceptionOnFailure) {
+            $output = $process->getErrorOutput() ? $process->getErrorOutput() : $process->getOutput();
+            $msg = sprintf('Error executing "%s": %s', $process->getCommandLine(), $output);
+
+            throw new \Exception($msg);
+        }
+
+        return $process->isSuccessful();
+    }
+
+    /**
+     * @return Process
+     */
+    private static function createProcess($cmd, $timeout = false)
+    {
+        $process = new Process($cmd);
+        $process->setWorkingDirectory(self::$workingDirectory);
+        if (false !== $timeout) {
+            $process->setTimeout($timeout);
+        }
+
+        return $process;
+    }
+
+    private static function getCallback()
+    {
+        if (self::$logger) {
+            $logger = self::$logger;
+            return function ($type, $line) use ($logger) {
+                if ($type === 'err') {
+                    $logger->error($line);
+                } else {
+                    $logger->debug($line);
+                }
+            };
+        }
+    }
+
+    protected static function executeWithRetry($cmd, $timeout = false, $retries = 3)
+    {
+        $process = self::createProcess($cmd, $timeout);
+        for ($i = 0; $i <= $retries; $i++) {
+            // only allow throwing exceptions on final attempt
+            $throwExceptionOnFailure = $i == $retries;
+            if (self::executeProcess($process, $throwExceptionOnFailure)) {
+                return true;
+            }
+            if (self::$logger && $i < $retries) {
+                self::$logger->debug('Retrying the command: ' . $cmd);
+            }
+        }
+        return false;
     }
 }
