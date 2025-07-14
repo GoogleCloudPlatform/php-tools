7029b183ae155a6f28dd465332d155d9fb718fa2
Brent Shaffer <betterbrent@google.com>
2025-07-14 14:23:44 -0700

    fix phpstan

--- src/Utils/Actions/RunCsFixerCommand.php
+++ src/Utils/Actions/RunCsFixerCommand.php
@@ -65,7 +65,7 @@ class RunCsFixerCommand extends Command
             );
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $repo = $input->getArgument('repo');
         if (false === strpos($repo, '/')) {
